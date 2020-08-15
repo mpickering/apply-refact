78071f8091de78dadf28727d11a4cb1f94e38b12
Ziyang Liu <unsafeFixIO@gmail.com>
2020-08-14 20:33:53 -0700

    Remove some exported functions from Refact.Apply which are only used in runPipe

--- src/Refact/Apply.hs
+++ src/Refact/Apply.hs
@@ -3,15 +3,6 @@
 module Refact.Apply
   ( runRefactoring
   , applyRefactorings
-
-  -- * Support for runPipe in the main process
-  , Verbosity(..)
-  , rigidLayout
-  , removeOverlap
-  , refactOptions
-  , type Errors
-  , onError
-  , mkErr
   ) where
 
 import Refact.Internal
